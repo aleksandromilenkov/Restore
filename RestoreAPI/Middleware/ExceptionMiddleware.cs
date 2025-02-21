6616c19a92d7e6332c26c3d3dfd775462873b0f3
Aleksandro <aleksandro.macedonia@gmail.com>
2025-02-22 00:28:34 +0100

    Creating Exception Middleware to catch and handle errors

---
+++ RestoreAPI/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,41 @@
+﻿
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Text.Json;
+
+namespace RestoreAPI.Middleware
+{
+    public class ExceptionMiddleware(IHostEnvironment env, ILogger<ExceptionMiddleware> logger) : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception ex) {
+                await HandleException(context, ex);
+            }
+        }
+
+        private async Task HandleException(HttpContext context, Exception ex)
+        {
+            logger.LogError(ex, ex.Message);
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            var response = new ProblemDetails()
+            {
+                Detail = env.IsDevelopment() ? ex.StackTrace?.ToString() : null,
+                Status = 500,
+                Title = ex.Message,
+            };
+
+            // correct format for a JSON response:
+            var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+            
+            var json = JsonSerializer.Serialize(response, options);
+            await context.Response.WriteAsync(json);
+        }
+    }
+}
--- RestoreAPI/Program.cs
+++ RestoreAPI/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using RestoreAPI.Data;
+using RestoreAPI.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,8 +11,13 @@ builder.Services.AddDbContext<StoreContext>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
+builder.Services.AddTransient<ExceptionMiddleware>();
 
 var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(options =>
 {
     options
@@ -19,7 +25,6 @@ app.UseCors(options =>
     .AllowAnyMethod()
     .WithOrigins("https://localhost:3000");
 });
-// Configure the HTTP request pipeline.
 app.MapControllers();
 
 DbInitializer.InitDb(app);
