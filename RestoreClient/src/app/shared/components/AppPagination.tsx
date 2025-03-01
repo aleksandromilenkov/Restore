27679f9f5655b6f9c315fbc00643fb2a682ddf81
Aleksandro <aleksandro.macedonia@gmail.com>
2025-03-01 21:59:17 +0100

    creating Pagination part 2, move the logic to separate component and update the Catalog.tsx to use the pagination accurately Co-authored-by: Eva Milenkova <emilenkova@yahoo.com>

---
+++ RestoreClient/src/app/shared/components/AppPagination.tsx
@@ -0,0 +1,31 @@
+import { Box, Pagination, Typography } from "@mui/material";
+import { Pagination as PaginationType } from "../../models/pagination";
+
+type Props = {
+    metadata: PaginationType
+    onPageChange: (page:number)=>void;
+};
+
+const AppPagination = ({metadata, onPageChange}: Props) => {
+    const {pageSize, currentPage, totalCount, totalPages} = metadata;
+    const startItem = (currentPage - 1) * pageSize +1;
+    const endItem = Math.min(currentPage * pageSize, totalCount);
+  return (
+    <Box
+      display="flex"
+      justifyContent="space-between"
+      alignItems={"center"}
+      mt={"3"}
+    >
+        <Typography>Displaying {startItem}-{endItem} of {totalCount} items</Typography>
+      <Pagination
+        color="secondary"
+        size="large"
+        count={totalPages}
+        page={currentPage}
+        onChange={(_, page)=>onPageChange(page)}
+      />
+    </Box>
+  );
+};
+export default AppPagination;
--- RestoreClient/src/features/catalog/Catalog.tsx
+++ RestoreClient/src/features/catalog/Catalog.tsx
@@ -1,26 +1,30 @@
-import { Grid2, Pagination } from "@mui/material";
+import { Grid2, Typography } from "@mui/material";
 import ProductList from "./ProductList";
 import { useFetchProductsQuery } from "./catalogApi";
 import Filters from "./Filters";
-import { useAppSelector } from "../../app/store/store";
+import { useAppDispatch, useAppSelector } from "../../app/store/store";
+import AppPagination from "../../app/shared/components/AppPagination";
+import { setPageNumber } from "./catalogSlice";
 
 const Catalog = () => {
   const productParams = useAppSelector((state) => state.catalogSlice);
+  const dispatch = useAppDispatch();
   const { data, isLoading } = useFetchProductsQuery(productParams);
   if (isLoading || !data) return <div>Loading...</div>;
+  const onPageChangeHandler = (page:number)=>{
+    dispatch(setPageNumber(page));
+  }
   return (
     <Grid2 container spacing={4}>
       <Grid2 size={3}>
         <Filters />
       </Grid2>
       <Grid2 size={9}>
+        {data.items && data.items.length > 0 ? (<>
         <ProductList products={data.items} />
-        <Pagination
-          color="secondary"
-          size="large"
-          count={data.pagination.totalPages}
-          page={data.pagination.currentPage}
-        />
+        <AppPagination metadata={data.pagination} onPageChange={onPageChangeHandler}/>
+        </>) : <Typography variant="h5">There are no results for this filter</Typography>}
+        
       </Grid2>
     </Grid2>
   );
